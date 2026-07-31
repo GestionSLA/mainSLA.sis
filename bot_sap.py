789b51272675ba2541ee6c0721ccf58760ad5fde
Gestion.SLA <productousb@gmail.com>
2026-07-31 11:28:09 -0300

    Update bot_sap.py

--- bot_sap.py
+++ bot_sap.py
@@ -582,6 +582,9 @@ def main():
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-software-rasterizer")
+    chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--window-size=2560,1440")
 
     driver = webdriver.Chrome(options=chrome_options)
